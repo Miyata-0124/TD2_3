6c998b8311b09003453a05c5c0ca633fd4881410
MiyataKouki <k021g1124@g.neec.ac.jp>
2022-12-08 16:10:22 +0900

    masterに一度合わせた

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -2,14 +2,11 @@
 #include "TextureManager.h"
 #include "Affine.h"
 #include <cassert>
-#include <MathUtility.h>
-using namespace MathUtility;
-// 回転処理
+
 GameScene::GameScene() {}
 
 GameScene::~GameScene() {
 	delete model_;
-	delete coreModel_;
 	delete debugCamera_;
 }
 
@@ -21,71 +18,113 @@ void GameScene::Initialize() {
 	debugText_ = DebugText::GetInstance();
 	debugCamera_ = new DebugCamera(WinApp::kWindowWidth, WinApp::kWindowHeight);
 
-	//Box
 	model_ = Model::CreateFromOBJ("Box", true);
+	worldTransformPearent_.Initialize();
+	worldTransformPearent_.scale_ = { 5.0f,5.0f,5.0f };
+
 	worldTransform_.Initialize();
-	worldTransform_.scale_ = { 5.0f,5.0f,5.0f };
-
-	//Core
-	textureHandle_ = TextureManager::Load("Core.png");
-	coreModel_ = Model::Create();
-	coreTransform_.Initialize();
-	coreTransform_.scale_ = { 0.2f,0.2f,0.2f };
-	coreTransform_.translation_ = { 0.0f,1.2f,0.0f };
-	coreTransform_.parent_ = &worldTransform_;
-
+	worldTransform_.scale_ = { 0.5f,0.5f,0.5f };
+	worldTransform_.translation_ = { 0.0f,1.2f,0.0f };
+	worldTransform_.parent_ = &worldTransformPearent_;
 
 	viewProjection_.Initialize();
-	Affine::CreateAffine(worldTransform_);
-	Affine::CreateAffine(coreTransform_);
+
+	Affine::CreateAffine(worldTransformPearent_, face);
+	Affine::CreateAffine(worldTransform_, face);
+	worldTransformPearent_.TransferMatrix();
 	worldTransform_.TransferMatrix();
-	coreTransform_.TransferMatrix();
 }
 
 void GameScene::Update() {
-	//キー操作
-#pragma region キー操作
-	if (input_->TriggerKey(DIK_W)) {
-		worldTransform_.rotation_ += {-(3.14f / 2.0f), 0.0f, 0.0f};
-		if (worldTransform_.rotation_.x < -4.71f)
+	if (input_->PushKey(DIK_UP)) {
+		viewProjection_.eye.y += 2.5f;
+		if (viewProjection_.eye.y > 50)
 		{
-			worldTransform_.rotation_.x = 0.0f;
+			viewProjection_.eye.y = 50;
+			viewProjection_.eye.z = -0.01;
 		}
 	}
+	else {
+		viewProjection_.eye = { 0, 0, -50 };
+		viewProjection_.target = { 0, 0, 0 };
+	}
+	if (input_->PushKey(DIK_LEFT)) {
+		viewProjection_.eye = { -50, 0, 0 };
+	}
+	if (input_->PushKey(DIK_DOWN)) {
+		viewProjection_.eye = { 0, -50, -0.01 };
+	}
+	if (input_->PushKey(DIK_RIGHT)) {
+		viewProjection_.eye = { 50, 0, 0 };
+	}
+	viewProjection_.UpdateMatrix();
+	const float radian = 8.0f;
+
 	if (input_->TriggerKey(DIK_A)) {
-		worldTransform_.rotation_ += {0.0f, -(3.14f / 2.0f), 0.0f};
-		if (worldTransform_.rotation_.y < -4.71f)
-		{
-			worldTransform_.rotation_.y = 0.0f;
-		}
-	}
-	if (input_->TriggerKey(DIK_S)) {
-		worldTransform_.rotation_ += {3.14f / 2.0f, 0.0f, 0.0f};
-		if (worldTransform_.rotation_.x > 4.71f)
-		{
-			worldTransform_.rotation_.x = 0.0f;
-		}
+		worldTransformPearent_.rotation_ = { 0.0f, 0.0f, PI / radian };
+		Affine::CreateMatRotZ(worldTransformPearent_, worldTransformPearent_.rotation_);
+
 	}
 	if (input_->TriggerKey(DIK_D)) {
-		worldTransform_.rotation_ += { 0.0f, 3.14f / 2.0f, 0.0f};
-		if (worldTransform_.rotation_.y > 4.71f)
-		{
-			worldTransform_.rotation_.y = 0.0f;
+		worldTransformPearent_.rotation_ = { 0.0f, 0.0f, -PI / radian };
+		Affine::CreateMatRotZ(worldTransformPearent_, worldTransformPearent_.rotation_);
+
+	}
+
+	if (input_->TriggerKey(DIK_W)) {
+		worldTransformPearent_.rotation_ = { PI / radian, 0.0f, 0.0f };
+		Affine::CreateMatRotX(worldTransformPearent_, worldTransformPearent_.rotation_);
+	}
+	if (input_->TriggerKey(DIK_S)) {
+		worldTransformPearent_.rotation_ = { -PI / radian, 0.0f, 0.0f };
+		Affine::CreateMatRotX(worldTransformPearent_, worldTransformPearent_.rotation_);
+	}
+
+	if (input_->TriggerKey(DIK_Q)) {
+		worldTransformPearent_.rotation_ += {0.0f, PI / radian, 0.0f};
+
+	}
+	if (input_->TriggerKey(DIK_E)) {
+		worldTransformPearent_.rotation_ -= {0.0f, PI / radian, 0.0f};
+	}
+
+	if (worldTransformPearent_.rotation_.x >= PI * 2 || worldTransformPearent_.rotation_.x <= -PI * 2) {
+		worldTransformPearent_.rotation_.x = 0.0f;
+	}
+
+	if (worldTransformPearent_.rotation_.z >= PI * 2 || worldTransformPearent_.rotation_.z <= -PI * 2) {
+		worldTransformPearent_.rotation_.z = 0.0f;
+	}
+
+	if (input_->TriggerKey(DIK_SPACE)) {
+		if (face == 0) {
+			face = 1;
+		}
+		else {
+			face = 0;
 		}
 	}
-	coreTransform_.rotation_ = worldTransform_.rotation_;
-	
 
+	if (input_->TriggerKey(DIK_X)) {
+		worldTransform_.translation_.y -= 0.1f;
+	}
+	if (worldTransform_.translation_.y < 1.2f && worldTransform_.translation_.y >= -1.2f) {
+		worldTransform_.translation_.y -= 0.1f;
+	}
+	worldTransformPearent_.TransferMatrix();
 
-#pragma endregion
-	Affine::CreateAffine(worldTransform_);
-	Affine::CreateAffine(coreTransform_);
-	//親子構造
-	coreTransform_.matWorld_ *= worldTransform_.matWorld_;
-
+	Affine::CreateAffine(worldTransform_, face);
+	worldTransform_.matWorld_ *= worldTransform_.parent_->matWorld_;
 	worldTransform_.TransferMatrix();
-	coreTransform_.TransferMatrix();
+
 	debugCamera_->Update();
+
+	debugText_->SetPos(20, 20);
+	debugText_->Printf("%f,%f,%f",
+		worldTransformPearent_.rotation_.x,
+		worldTransformPearent_.rotation_.y,
+		worldTransformPearent_.rotation_.z);
+
 }
 
 void GameScene::Draw() {
@@ -98,7 +137,6 @@ void GameScene::Draw() {
 	Sprite::PreDraw(commandList);
 
 	/// <summary>
-	/// 
 	/// ここに背景スプライトの描画処理を追加できる
 	/// </summary>
 
@@ -115,8 +153,9 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-	model_->Draw(worldTransform_, debugCamera_->GetViewProjection());
-	coreModel_->Draw(coreTransform_, debugCamera_->GetViewProjection(),textureHandle_);
+	model_->Draw(worldTransformPearent_, viewProjection_);
+	model_->Draw(worldTransform_, viewProjection_);
+
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
@@ -128,19 +167,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-#pragma region デバッグテキスト
 
-	debugText_->SetPos(10, 10);
-	debugText_->Printf("BoxRotation{%f,%f,%f}\n", worldTransform_.rotation_.x, worldTransform_.rotation_.y, worldTransform_.rotation_.z);
-	debugText_->SetPos(10, 30);
-	debugText_->Printf("BoxTranslation{%f,%f,%f}\n", worldTransform_.translation_.x, worldTransform_.translation_.y, worldTransform_.translation_.z);
-	debugText_->SetPos(400, 10);
-	debugText_->Printf("CoreRotation{%f,%f,%f}\n", coreTransform_.rotation_.x, coreTransform_.rotation_.y, coreTransform_.rotation_.z);
-	debugText_->SetPos(400, 30);
-	debugText_->Printf("CoreTranslation{%f,%f,%f}\n", coreTransform_.translation_.x, coreTransform_.translation_.y, coreTransform_.translation_.z);
-
-#pragma endregion
-	
 	// デバッグテキストの描画
 	debugText_->DrawAll(commandList);
 	//
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -16,7 +16,7 @@
 /// </summary>
 class GameScene {
 
-  public: // メンバ関数
+public: // メンバ関数
 	/// <summary>
 	/// コンストクラタ
 	/// </summary>
@@ -42,7 +42,7 @@ class GameScene {
 	/// </summary>
 	void Draw();
 
-  private: // メンバ変数
+private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
